9f6e94e1bd0b679956c505477b48f4e38b0e22cd
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-06-11 22:34:18 +0000

    minor refactoring; new option: ASSETS_AUTO_CREATE

--- django_assets/templatetags/assets.py
+++ django_assets/templatetags/assets.py
@@ -16,15 +16,17 @@ def _absurl(fragment):
 
 def _abspath(filename):
     """Create an absolute path based on MEDIA_ROOT."""
+    if os.path.isabs(filename):
+        return filename
     return os.path.join(settings.MEDIA_ROOT, filename)
 
-def create_merged(sources, output, filter, force=False):
+def create_merged(sources, output, filter):
     """Does the templatetag's heavy lifting: Merges multiple source files
     into the output file, while applying the specified filter. Uses an
     existing output file whenever possible.
 
-    If ``force`` is enabled, the resources will always be recreated, and
-    the check, whether an update is required, is skipped.
+    The ``output_path`` and ``source_paths`` arguments can be relative, or
+    absolute, we handle both.
     """
 
     # fail early by resolving filters now (there can be multiple)
@@ -37,74 +39,65 @@ def create_merged(sources, output, filter, force=False):
     output_filters = [f for f in filters if not getattr(f, 'is_source_filter', False)]
     source_filters = [f for f in filters if getattr(f, 'is_source_filter', False)]
 
-    # make paths absolute
+    # make paths absolute (they might already be, we can't be sure)
     output_path = _abspath(output)
     source_paths = [_abspath(s) for s in sources]
 
-    # check if the asset needs to be (re)created
-    if force or not os.path.exists(output_path):
-        update_needed = True
-    else:
-        update_needed = get_updater()(output_path, source_paths)
+    # TODO: is it possible that another simultaneous request might
+    # cause trouble? how would we avoid this?
+    open_output = lambda: open(output_path, 'wb')
 
-    if update_needed:
-        # TODO: is it possible that another simultaneous request might
-        # cause trouble? how would we avoid this?
-        open_output = lambda: open(output_path, 'wb')
-
-        # If no output filters is used, we can write directly to the
-        # disk for improved performance.
-        buf = (output_filters) and StringIO.StringIO() or open_output()
+    # If no output filters is used, we can write directly to the
+    # disk for improved performance.
+    buf = (output_filters) and StringIO.StringIO() or open_output()
+    try:
         try:
-            try:
-                for source in source_paths:
-                    _in = open(source, 'rb')
-                    try:
-                        # apply source filters
-                        if source_filters:
-                            tmp_buf = _in  # first filter reads directly from input
-                            for filter in source_filters[:-1]:
-                                tmp_out = StringIO.StringIO()
-                                filter.apply(tmp_buf, tmp_out, source, output_path)
-                                tmp_buf = tmp_out
-                                tmp_buf.seek(0)
-                            # Let last filter write directly to final buffer
-                            # for performance, instead of copying once more.
-                            for filter in source_filters[-1:]:
-                                filter.apply(tmp_buf, buf, source, output_path)
-                        else:
-                            buf.write(_in.read())
-                        buf.write("\n")  # can be important, depending on content
-                    finally:
-                        _in.close()
-
-                # apply output filters, copy from "buf" to "out" (the file)
-                if output_filters:
-                    out = open_output()
-                    try:
-                        buf.seek(0)
-                        for filter in output_filters[:-1]:
+            for source in source_paths:
+                _in = open(source, 'rb')
+                try:
+                    # apply source filters
+                    if source_filters:
+                        tmp_buf = _in  # first filter reads directly from input
+                        for filter in source_filters[:-1]:
                             tmp_out = StringIO.StringIO()
-                            filter.apply(buf, tmp_out)
-                            buf = tmp_out
-                            buf.seek(0)
-                        # Handle the very last filter separately, let it
-                        # write directly to output file for performance.
-                        for filter in output_filters[-1:]:
-                            filter.apply(buf, out)
-                    finally:
-                        out.close()
-            finally:
-                buf.close()
-        except Exception:
-            # If there was an error above make sure we delete a possibly
-            # partly created output file, or it might be considered "done"
-            # from now on.
-            if os.path.exists(output_path):
-                os.remove(output_path)
-            raise
+                            filter.apply(tmp_buf, tmp_out, source, output_path)
+                            tmp_buf = tmp_out
+                            tmp_buf.seek(0)
+                        # Let last filter write directly to final buffer
+                        # for performance, instead of copying once more.
+                        for filter in source_filters[-1:]:
+                            filter.apply(tmp_buf, buf, source, output_path)
+                    else:
+                        buf.write(_in.read())
+                    buf.write("\n")  # can be important, depending on content
+                finally:
+                    _in.close()
 
-    return os.stat(output_path).st_mtime
+            # apply output filters, copy from "buf" to "out" (the file)
+            if output_filters:
+                out = open_output()
+                try:
+                    buf.seek(0)
+                    for filter in output_filters[:-1]:
+                        tmp_out = StringIO.StringIO()
+                        filter.apply(buf, tmp_out)
+                        buf = tmp_out
+                        buf.seek(0)
+                    # Handle the very last filter separately, let it
+                    # write directly to output file for performance.
+                    for filter in output_filters[-1:]:
+                        filter.apply(buf, out)
+                finally:
+                    out.close()
+        finally:
+            buf.close()
+    except Exception:
+        # If there was an error above make sure we delete a possibly
+        # partly created output file, or it might be considered "done"
+        # from now on.
+        if os.path.exists(output_path):
+            os.remove(output_path)
+        raise
 
 
 class AssetsNode(template.Node):
@@ -148,7 +141,24 @@ class AssetsNode(template.Node):
         """Create and output a merged version of all sources."""
 
         output, files, filter = self.resolve(context)
-        last_modified = create_merged(files, output, filter)
+
+        # make paths absolute
+        output_path = _abspath(output)
+        source_paths = [_abspath(s) for s in files]
+
+        # check if the asset should be (re)created
+        if not os.path.exists(output_path):
+            if not settings.ASSETS_AUTO_CREATE:
+                # render the sources after all
+                return self.render_sources(context)
+            else:
+                update_needed = True
+        else:
+            update_needed = get_updater()(output_path, source_paths)
+
+        if update_needed:
+            create_merged(source_paths, output_path, filter)
+        last_modified = os.stat(output_path).st_mtime
         # TODO: do asset tracking here
         #get_tracker()()
 
