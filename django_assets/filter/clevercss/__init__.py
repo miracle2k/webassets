3eaa944b6ef513682aace4569a2a84e0d1698120
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-08-18 22:59:12 +0200

    added a clevercss filter; thanks to Florian Schlachter

---
+++ django_assets/filter/clevercss/__init__.py
@@ -0,0 +1,13 @@
+﻿"""Converts CleverCSS markup to real CSS.
+
+If you want to combine this with other CSS filters, make sure this
+one runs first.
+
+For more information about CleverCSS, see:
+    http://sandbox.pocoo.org/clevercss/
+"""
+
+import clevercss
+
+def apply(_in, out):
+	out.write(clevercss.convert(_in.read()))
