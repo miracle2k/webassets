8cc0aa98fb6dadc38b55c9c139a58bc3c2e06924
Michael Elsdoerfer <michael@elsdoerfer.info>
2009-10-10 11:52:39 +0200

    Fixed typo.

--- django_assets/filter/jsmin/__init__.py
+++ django_assets/filter/jsmin/__init__.py
@@ -1,8 +1,8 @@
 """Minifies Javascript by removing whitespace, comments, etc.
 
-Based on Baruch Even's port of Douglas Crockford's `JSMin 
-<http://www.crockford.com/javascript/jsmin.html>`_, which is 
-included, so no external dependancy is required.
+Based on Baruch Even's port of Douglas Crockford's `JSMin
+<http://www.crockford.com/javascript/jsmin.html>`_, which is
+included, so no external dependency is required.
 """
 
 from jsmin import JavascriptMinify
