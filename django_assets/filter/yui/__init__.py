e127c943198615a5f3936839289db2306c2c2b88
Michael Elsdoerfer <michael@elsdoerfer.info>
2009-10-15 13:22:22 +0200

    Fixed 'yui' filter.

--- django_assets/filter/yui/__init__.py
+++ django_assets/filter/yui/__init__.py
@@ -1,18 +1,18 @@
-"""Minify Javascript and CSS with 
+"""Minify Javascript and CSS with
 `YUI Compressor <http://developer.yahoo.com/yui/compressor/>`_.
 
-This filter defaults to JS mode, but it is recommended that you explicitly 
+This filter defaults to JS mode, but it is recommended that you explicitly
 specify ``yui_js`` instead. Use ``yui_css`` to compress CSS files.
 
-YUI Compressor is an external tool written in Java, which needs to be 
-available. You can define a ``YUI_COMPRESSOR_PATH`` setting that points to 
-the ``.jar`` file. Otherwise, an environment variable by the same name is 
-tried. The filter will also look for a ``JAVA_HOME`` environment variable 
-to run the ``.jar`` file, or will otherwise assume that ``java`` is on the 
+YUI Compressor is an external tool written in Java, which needs to be
+available. You can define a ``YUI_COMPRESSOR_PATH`` setting that points to
+the ``.jar`` file. Otherwise, an environment variable by the same name is
+tried. The filter will also look for a ``JAVA_HOME`` environment variable
+to run the ``.jar`` file, or will otherwise assume that ``java`` is on the
 system path.
 """
 
 from django_assets.filter import yui_js
 
 def apply(_in, out):
-    return yui.apply(_in, out, mode='js')
+    return yui_js.apply(_in, out)
