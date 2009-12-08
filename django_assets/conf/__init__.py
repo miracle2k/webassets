a57eb96b529e4a9a1feaf0ed71bbe23a9c627a09
Michael Elsdoerfer <michael@elsdoerfer.info>
2009-12-08 05:19:30 +0100

    Rewrote our config module to be a more dynamic (now no longer copying the Django settings object, but always accessing it on demand). It has the added benefit of us now avoiding Django bug #12336.

--- django_assets/conf/__init__.py
+++ django_assets/conf/__init__.py
@@ -1,9 +1,18 @@
 import default as default_settings
 from django.conf import settings as django_settings
 
-from django.conf import UserSettingsHolder
-settings = UserSettingsHolder(default_settings)
-for name in dir(django_settings):
-    if name == name.upper():
-        value = getattr(django_settings, name)
-        setattr(settings, name, value)
+class WrappedSettings(object):
+    """Wraps around the Django settings, falling back to our own
+    default settings module.
+    """
+
+    def __init__(self, *setting_objs):
+        self.setting_objs = setting_objs
+
+    def __getattr__(self, name):
+        for settings in self.setting_objs:
+            if hasattr(settings, name):
+                return getattr(settings, name)
+        raise AttributeError()
+
+settings = WrappedSettings(django_settings, default_settings)
