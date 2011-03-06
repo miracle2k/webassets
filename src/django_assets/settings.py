d9b6f90fb326cc805bd703cb0eb60a421f16970a
Michael Elsdoerfer <michael@elsdoerfer.com>
2011-03-06 18:37:19 +0100

    Explain that ASSETS_ROOT and ASSET_URL will point to Django settings values by default.

--- src/django_assets/settings.py
+++ src/django_assets/settings.py
@@ -9,9 +9,24 @@ the actual docstrings.
 
 from webassets import Environment
 
+class docwrap(object):
+    def __init__(self, object, append=None):
+        self.__doc__ = object.__doc__
+        if append:
+            # Add to the docstring, maintaining the proper indentation
+            # so that the reST will be formatted correctly.
+            last_line = self.__doc__.splitlines()[-1]
+            indent = last_line[-len(last_line.lstrip()):]
+            append = "\n".join(map(lambda s: indent+s, append.splitlines()))
+            self.__doc__ += append
+
+
 ASSETS_DEBUG = Environment.debug
 ASSETS_CACHE = Environment.cache
 ASSETS_UPDATER = Environment.updater
 ASSETS_EXPIRE = Environment.expire
-ASSETS_URL = Environment.url
-ASSETS_ROOT = Environment.directory
+ASSETS_URL = docwrap(Environment.url, """\n\nBy default, ``STATIC_URL``
+will be used for this, or the older ``MEDIA_URL`` setting.""")
+ASSETS_ROOT = docwrap(Environment.directory, """\n\nBy default,
+``STATIC_ROOT`` will be used for this, or the older ``MEDIA_ROOT``
+setting.""")
