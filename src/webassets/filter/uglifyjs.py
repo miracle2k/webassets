13cad8b6fb7c484a492333e86a6e774ce4742a40
Bryan Chow <bryan@fullfactor.com>
2011-11-04 13:47:04 -0700

    Make UglifyJSFilter docstring more consistent with other filters

--- src/webassets/filter/uglifyjs.py
+++ src/webassets/filter/uglifyjs.py
@@ -1,11 +1,3 @@
-"""Minify Javascript using `UglifyJS <https://github.com/mishoo/UglifyJS/>`_.
-
-UglifyJS is an external tool written for NodeJS; this filter assumes that
-the ``uglifyjs`` executable is in the path. Otherwise, you may define
-a ``UGLIFYJS_BIN`` setting. Additional options may be passed to ``uglifyjs``
-by setting ``UGLIFYJS_EXTRA_ARGS``, which expects a list of strings.
-"""
-
 import subprocess
 from webassets.exceptions import FilterError
 from webassets.filter import Filter
@@ -15,6 +7,16 @@ __all__ = ('UglifyJSFilter',)
 
 
 class UglifyJSFilter(Filter):
+    """
+    Minify Javascript using `UglifyJS <https://github.com/mishoo/UglifyJS/>`_.
+
+    UglifyJS is an external tool written for NodeJS; this filter assumes that
+    the ``uglifyjs`` executable is in the path. Otherwise, you may define
+    a ``UGLIFYJS_BIN`` setting.
+
+    Additional options may be passed to ``uglifyjs`` using the setting
+    ``UGLIFYJS_EXTRA_ARGS``, which expects a list of strings.
+    """
 
     name = 'uglifyjs'
 
