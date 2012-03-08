b56a1bf5e90a76251231762cccf2089d634c10bf
Erik Simmler <tgecho@gmail.com>
2012-03-07 20:07:45 -0500

    AssetsFinder with better support for CachedStaticFileStorage

--- src/django_assets/finders.py
+++ src/django_assets/finders.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import staticfiles
+from django.contrib.staticfiles.utils import matches_patterns
 from django.core.files.storage import FileSystemStorage
 from django_assets.env import get_env
 
@@ -25,3 +27,16 @@ class AssetsFinder(staticfiles.finders.BaseStorageFinder):
     # staticfiles is smart enough to recognize that the files have
     # not changed (being the same files), and doesn't overwrite them.
     storage = AssetsFileStorage
+
+    def list(self, ignore_patterns):
+        env = get_env()
+        if env.directory == getattr(settings, 'STATIC_ROOT'):
+            for bundle in env:
+                if not matches_patterns(bundle.output, ignore_patterns) and \
+                 self.storage.exists(bundle.output):
+                    yield bundle.output, self.storage
+        else:
+            # We can't just return the list since return can't coexist
+            # with above yield
+            for output in super(AssetsFinder, self).list(ignore_patterns):
+                yield output
