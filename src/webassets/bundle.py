miracle2k/webassets

commit 815282c6767b39e7b3dd8bac08cbac56789fa7ff

Fixed input() kwargs when running after open().
There was a bug where an input() filter would not receive the source_path kwarg
if an open() filter had been run before. This was because source_path was taken
strictly from the FileHunk.filename attribute, breaking when a previous filter
cause a MemoryHunk to be used.

One problem caused by this was the cssrewrite filter being broken.

src/webassets/bundle.py
@@ -376,11 +376,10 @@ class Bundle(object):
                     combined_filters, disable_cache=disable_cache)
                 hunks.append(hunk)
             else:
-                # Pass along the original relative path, as specified by the
-                # user. This may differ from the actual filesystem path, if
-                # extensions provide a virtualized filesystem (e.g. Flask
-                # blueprints, Django staticfiles).
-                kwargs = {'source': rel_name}
+                # To the input() and output() methods, pass along both the
+                # original relative path, as specified by the user, and the
+                # one that has been resolved to a filesystem location.
+                kwargs = {'source': rel_name, 'source_path': item}
 
                 # Give a filter the chance to open his file.
                 try:

src/webassets/merge.py
@@ -190,8 +190,6 @@ class FilterTool(object):
         def func():
             kwargs_final = self.kwargs.copy()
             kwargs_final.update(kwargs or {})
-            if hasattr(hunk, 'filename'):
-                kwargs_final.setdefault('source_path', hunk.filename)
 
             data = StringIO.StringIO(hunk.data())
             for filter in filters:

tests/test_bundle.py
@@ -456,6 +456,23 @@ class TestFilters(TempEnvironmentHelper):
         self.mkbundle('a', 'b', filters=ConcatFilter, output='out').build()
         assert self.get('out') == '1%%%2'
 
+    def test_open_before_input(self):
+        """[Regression] Test that if an open filter is used, input filters
+        still receive the ``source_path`` kwargs.
+        """
+        captured_kw = {}
+        class TestFilter(Filter):
+            def open(self, out, *a, **kw): out.write('foo')
+            def input(self, *a, **kw):
+                assert not captured_kw
+                captured_kw.update(kw)
+        self.create_files({'a': '1'})
+        self.mkbundle('a', filters=TestFilter, output='out').build()
+        # TODO: Could be generalized to test all the other values that
+        # each filter method expects to receive. This is currently not
+        # done anywhere )though it likely still wouldn't have caught this).
+        assert 'source_path' in captured_kw
+
 
 class TestAutoUpdate(TempEnvironmentHelper):
     """Test bundle auto rebuild, and generally everything involving

commit b461d31caed505e28942db7af25dd2194bd5207f

815282c broke open() filters.
The tests caught this correctly.

src/webassets/bundle.py
@@ -376,15 +376,13 @@ class Bundle(object):
                     combined_filters, disable_cache=disable_cache)
                 hunks.append(hunk)
             else:
-                # To the input() and output() methods, pass along both the
-                # original relative path, as specified by the user, and the
-                # one that has been resolved to a filesystem location.
-                kwargs = {'source': rel_name, 'source_path': item}
-
                 # Give a filter the chance to open his file.
                 try:
                     hunk = filtertool.apply_func(
-                        combined_filters, 'open', [item], kwargs=kwargs)
+                        combined_filters, 'open', [item],
+                        # Also pass along the original relative path, as
+                        # specified by the user, before resolving.
+                        kwargs={'source': rel_name})
                 except MoreThanOneFilterError, e:
                     raise BuildError(e)
 
@@ -398,7 +396,11 @@ class Bundle(object):
                     hunks.append(hunk)
                 else:
                     hunks.append(filtertool.apply(
-                        hunk, combined_filters, 'input', kwargs=kwargs))
+                        hunk, combined_filters, 'input',
+                        # Pass along both the original relative path, as
+                        # specified by the user, and the one that has been
+                        # resolved to a filesystem location.
+                        kwargs={'source': rel_name, 'source_path': item}))
 
         # Merge the individual files together.
         try: