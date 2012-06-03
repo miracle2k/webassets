miracle2k/webassets

commit eadcff78686de9b4429139e1083f294ca2b461e3

Improve CLI logger setup.
Don't attach multiple handlers, only modify the handler we added ourselves.

src/webassets/script.py
@@ -380,10 +380,17 @@ class GenericArgparseImplementation(object):
         if self.log:
             log = self.log
         else:
-            log = logging.getLogger('webassets')
-            log.setLevel(logging.DEBUG if ns.verbose else (
-                logging.WARNING if ns.quiet else logging.INFO))
-            log.addHandler(logging.StreamHandler())
+            log = logging.getLogger('webassets.script')
+            if not log.handlers:
+                # In theory, this could run multiple times (e.g. tests)
+                handler = logging.StreamHandler()
+                log.addHandler(handler)
+                # Note that setting the level filter at the handler level is
+                # better than the logger level, since this is "our" handler,
+                # we create it, for the purposes of having a default output.
+                # The logger itself the user may be modifying.
+                handler.setLevel(logging.DEBUG if ns.verbose else (
+                    logging.WARNING if ns.quiet else logging.INFO))
         return log
 
     def _setup_assets_env(self, ns, log):

commit 132226a7b38cb90803ce29f95ec841c549cfdb5a

Fixed CLI logging output.
eadcff78686de9b4429139e1083f294ca2b461e3 broke the command line output due to
the wrong effective logging level being used.

src/webassets/script.py
@@ -18,6 +18,11 @@ from webassets.merge import MemoryHunk
 __all__ = ('CommandError', 'CommandLineEnvironment', 'main')
 
 
+# logging has WARNING as default level, for the CLI we want INFO. Set this
+# as early as possible, so that user customizations will not be overwritten.
+logging.getLogger('webassets.script').setLevel(logging.INFO)
+
+
 class CommandError(Exception):
     pass
 