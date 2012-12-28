bae89d346759388dbc99377759acb81464bc86a1
Erik Simmler <tgecho@gmail.com>
2012-12-28 14:45:51 -0500

    Make jinja2 run as an import filter

--- src/webassets/filter/jinja2.py
+++ src/webassets/filter/jinja2.py
@@ -9,6 +9,13 @@ class Jinja2(Filter):
     """Process a file through the Jinja2 templating engine.
 
     Requires the ``jinja2`` package (https://github.com/mitsuhiko/jinja2).
+
+    The Jinja2 context can be specified with the `JINJA2_CONTEXT` configuration
+    option or directly with `context={...}`. Example:
+
+    .. code-block:: python
+
+        Bundle('input.css', filters=Jinja2(context={'foo': 'bar'}))
     """
 
     name = 'jinja2'
@@ -26,5 +33,5 @@ class Jinja2(Filter):
             self.jinja2 = jinja2
         super(Jinja2, self).setup()
 
-    def output(self, _in, out, **kw):
+    def input(self, _in, out, **kw):
         out.write(self.jinja2.Template(_in.read()).render(self.context or {}))
