e2c65904b199b9be0126bd424f8fb59c8ded31b6
Michael Elsdörfer <michael@elsdoerfer.com>
2014-06-29 23:32:01 +0200

    See if installing non sudo npm is possible.

--- requirements-dev.sh
+++ requirements-dev.sh
@@ -6,7 +6,7 @@ gem install sass --version 3.2.19
 gem install compass --version 0.12.6
 gem install less --version 1.2.21
 
-sudo npm install -g less
+npm install -g less
 sudo npm install -g uglify-js@2.3.1
 sudo npm install -g coffee-script@1.6.2
 sudo npm install -g clean-css@1.0.2
