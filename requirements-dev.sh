f332a22158c8970fa8beff9da261a08c95b62cfc
Michael Elsdörfer <michael@elsdoerfer.com>
2014-06-29 23:44:17 +0200

    Think we have it down now.

--- requirements-dev.sh
+++ requirements-dev.sh
@@ -4,14 +4,14 @@
 
 gem install sass --version 3.2.19
 gem install compass --version 0.12.6
-gem install less --version 1.2.21
+# Only install NodeJS version by default.
+#gem install less --version 1.2.21
 
 npm install -g less
-sudo npm install -g uglify-js@2.3.1
-sudo npm install -g coffee-script@1.6.2
-sudo npm install -g clean-css@1.0.2
-sudo npm install -g stylus
-sudo npm install -g handlebars
-sudo npm install -g typescript
-sudo npm install -g requirejs@2.1.11
-
+npm install -g uglify-js@2.3.1
+npm install -g coffee-script@1.6.2
+npm install -g clean-css@1.0.2
+npm install -g stylus
+npm install -g handlebars
+npm install -g typescript
+npm install -g requirejs@2.1.11
