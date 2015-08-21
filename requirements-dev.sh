e66818a5af43b63f302c9cd94bdee269ce2b5fc3
Michael Elsdörfer <michael@elsdoerfer.com>
2015-08-21 11:53:33 +0430

    sass has issues on Travis CI, again.

--- requirements-dev.sh
+++ requirements-dev.sh
@@ -2,8 +2,11 @@
 
 # External filter binaries to install for testing.
 
-gem install sass --version 3.2.19
-gem install compass --version 0.12.6
+# Disable, because there are issues with the install on Travis CI.
+#gem install sass --version 3.2.19
+#gem install compass --version 0.12.6
+
+
 # Only install NodeJS version by default.
 #gem install less --version 1.2.21
 
